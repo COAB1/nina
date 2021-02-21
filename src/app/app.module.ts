11e5f5e5957cf42ae1f7d5565e5dc9ca892422d4
Carolina Brito <carol.o.ag@gmail.com>
2021-02-21 20:54:16 -0300

    Cria componente e rota da pagina principal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule, 
